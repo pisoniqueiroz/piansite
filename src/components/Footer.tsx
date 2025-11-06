c7a4edb85fb3aba4ad3a1aaa40f16fdc191b1a00
pisoniqueiroz <pisoniqueiroz@gmail.com>
2025-11-06 18:31:52 -0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="text-white bg-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1.5fr_1fr_1fr_1.5fr] gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1.5fr_0.8fr_0.8fr_1.5fr] gap-12">
           {/* Logo e Descrição */}
           <div>
             <div className="flex items-center justify-start mb-6">
